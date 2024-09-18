e21cf690674bbc9aa5184e2565aa166c0ab852e7
RyanLanz123 <ryanlanz6@gmail.com>
2024-09-18 07:28:38 +0100

    setCount/prevCount

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = () => {
-  return (
-    <>
-    <h1>Name: John</h1>
-    <h2>Last Name: Doe</h2>
-    <h2>Age: 30</h2>
-    </>
-  )
-}
+
 
 const App = () => {
-  const name ='Jane';
-
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <Person />
+        <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
