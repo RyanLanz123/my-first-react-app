21cd0a9a0eb8ade75930b9b7ed041113d0e8712d
RyanLanz123 <ryanlanz6@gmail.com>
2024-09-19 09:25:20 +0100

    dynamically pass movies as prop to MovieCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useEffect} from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 // 424fce3d
 const API_URL = 'http://www.omdbapi.com?apikey=424fce3d';
 
@@ -13,11 +14,12 @@ const movie1 = {
 }
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json();
-            console.log(data);
+            setMovies(data.Search);
     }
     
 
@@ -42,22 +44,20 @@ const App = () => {
                 />
             </div>
 
-            <div className="container">
-                <div className='movie'>
-                    <div>
-                        <p>{movie1.year}</p>
+            {
+                movies?.length > 0
+                    ?(
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
                     </div>
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-                </div>
-            </div>
-
+                    ) : (
+                        <div className='empty'>
+                            <h2>No Movies Found</h2>
+                        </div>
+                    )
+            }
         </div>
     );
 }
